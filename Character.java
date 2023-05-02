9e386a12cbc5e1c002bbbe9176a6af8a0905a349
suryasuryasiva <suryasuryasiva001@gmail.com>
2023-05-02 21:51:10 +0530

    important

---
+++ Character.java
@@ -0,0 +1,31 @@
+package org.chara;
+
+public class Character {
+	
+	public static void main(String[] args) {
+		
+		
+		char char1 = 'a';
+		char char2 = 'A';
+	
+	System.out.println("char1:"+char1);
+	System.out.println("char2:"+char2);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
